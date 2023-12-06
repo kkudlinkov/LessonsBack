9a2db3a774421db35295418abe7535cd4bad1d56
Nikolai Kudlinkov <kkudlinkov@mail.ru>
2023-12-06 14:09:15 +0300

    Базовая конфигурация

---
+++ src/main/java/com/example/lessonsback/LessonsBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.lessonsback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonsBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LessonsBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lessonsback/LessonsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lessonsback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonsBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
